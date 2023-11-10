3329e1409b5c30036aff4fb1990623a778a2523c
Mike Stilllling <mike@stillingdigital.com>
2023-11-10 06:01:36 -0600

    homepage hero animation

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,20 +1,34 @@
 import barba from "@barba/core";
 
-// Behaviors
-import './interactions/accordion.js';
-
-// Transitions
+// Page Transitions and Scripts
 import {enterDefault, leaveDefault} from './transitions/default.js';
+import {enterHome, leaveHome} from './transitions/home.js';
+
+barba.hooks.enter(() => {
+    window.scrollTo(0, 0);
+});
 
 barba.init({
     transitions: [
         {
+            name: 'default',
             once({next}) {
                 enterDefault(next.container)
             },
             leave: ({current}) => leaveDefault(current.container),
             enter({next}) {
                 enterDefault(next.container)
+            }
+        },
+        {
+            name: 'home',
+            to: { namespace: ['home'] },
+            once({next}) {
+                enterHome(next.container)
+            },
+            leave: ({current}) => leaveHome(current.container),
+            enter({next}) {
+                enterHome(next.container)
             }
         }
     ]
