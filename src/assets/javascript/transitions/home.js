7ea1b4be9ca5cb6f08d4a54f1978b601e7a9e03e
Mike Stilllling <mike@stillingdigital.com>
2023-11-28 11:42:39 -0600

    add image

--- src/assets/javascript/transitions/home.js
+++ src/assets/javascript/transitions/home.js
@@ -4,6 +4,24 @@ import { SplitText } from "gsap/SplitText";
 import {accordion} from '../interactions/accordion.js';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
+function homeScroll() {
+    let mm = gsap.matchMedia();
+    mm.add("(min-width: 768px)", () => {
+        const tl = gsap.timeline({
+            defaults: { overwrite: true },
+            scrollTrigger: {
+                trigger: `[data-home-hero]`,
+                start: `top top`,
+                end: `bottom top`,
+                scrub: 1
+            },
+        });
+        tl
+            .to("[data-home-hero-img]", {y: '-43.75%'})
+    });
+}
+
+
 export const onceHome = (container) => {
     function enter() {
         const headline = new SplitText(document.querySelector("[data-headline]"), {type:"words"});
@@ -16,9 +34,10 @@ export const onceHome = (container) => {
                 .from(headline.words, {autoAlpha: 0, y: 36, duration:2, ease: "power4.out", stagger:0.05}, 0.25)
                 .from(subhead.lines, {autoAlpha: 0, y: 24, duration:2, ease: "power4.out", stagger:0.1}, 0.5)
                 .from("[data-dash]", {width: 0, duration:0.75, ease: "power4.out"}, 1)
+                .from("[data-intro]", {y: 16, autoAlpha:0, duration:1, ease: "power4.out", stagger:0.1}, 0.75)
                 .from("[data-caption]", {y: 8, autoAlpha:0, duration:1, ease: "power4.out", stagger:0.1}, 1)
                 .call(() => { headline.revert(); subhead.revert(); });
-            
+        homeScroll();
         accordion();
     }
     return enter();
@@ -35,9 +54,10 @@ export const enterHome = (container) => {
                 .from(headline.words, {autoAlpha: 0, y: 36, duration:2, ease: "power4.out", stagger:0.05}, 0.25)
                 .from(subhead.lines, {autoAlpha: 0, y: 24, duration:2, ease: "power4.out", stagger:0.1}, 0.5)
                 .from("[data-dash]", {width: 0, duration:0.75, ease: "power4.out"}, 1)
+                .from("[data-intro]", {y: 16, autoAlpha:0, duration:1, ease: "power4.out", stagger:0.1}, 0.75)
                 .from("[data-caption]", {y: 8, autoAlpha:0, duration:1, ease: "power4.out", stagger:0.1}, 1)
                 .call(() => { headline.revert(); subhead.revert(); });
-            
+        homeScroll();
         accordion();
     }
     return enter();
